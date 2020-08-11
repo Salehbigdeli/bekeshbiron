8f19fd24b73e6be6590a8748c3e01e846b7c072f
ali <alisaleh65@yahoo.com>
2020-08-12 02:44:03 +0430

    Change license

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
